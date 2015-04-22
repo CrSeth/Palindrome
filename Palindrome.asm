0587722231c4994c3ff4e61447ae1890ee8d47b3
Seth Stalley <sethstalley@gmail.com>
2015-04-21 21:36:58 -0600

    commented every line

--- Palindrome.asm
+++ Palindrome.asm
@@ -9,14 +9,14 @@
 %include "io.mac"
 
 .DATA				;Initialize Data
-instructionMsg	db	"Enter a phrase to check if it is a palindrome: ", 0		
-isPalindrome	db	"The phrase IS a palindrome!", 0
-notPalindrome	db	"The phrase is NOT a palindrome!", 0
+instructionMsg	db	"Enter a phrase to check if it is a palindrome: ", 0		;initial instruction
+isPalindrome	db	"The phrase IS a palindrome!", 0				;if phrase is a palindrome show this
+notPalindrome	db	"The phrase is NOT a palindrome!", 0				;else show this
 .UDATA
 phrase		resb	16	;buffer to store word can't be over 15chars
 .CODE				;Code area
-	.STARTUP		
-	PutStr	instructionMsg	
+	.STARTUP		;Code starts here	
+	PutStr	instructionMsg	;show instruction to user
 	GetStr	phrase,16	;read the word
 	
 	mov	esi, phrase	;initialize pointesr to beginning of phrase
@@ -28,34 +28,33 @@ len:				;chars in phrase
 	je	decEsi		;end len
 	inc	esi		;increment index
 	inc	ecx		;inc counter
-	jne 	len		
+	jne 	len		;keep looping if not at end of phrase
 decEsi:
 	dec	esi		;don't want 0
-	shr	ecx, 1
+	shr	ecx, 1		;cut the "len" in half because we are moving in from both sides
 reverse:	
 	mov	al, [esi]	;move last char to al
 	mov	ah, [ebx]	;move fist char to ah
 	
 	dec	esi		;decrease the counter and end char pointer
-	dec	ecx
+	dec	ecx	
 	inc	ebx		;inc the front running pointer
-	PutInt	cx
 	
 	or	al, 20h		;change 5bit to 1 | Lower Case ASCII
-	or	ah, 20h		
+	or	ah, 20h		;do same for other char
 
 	cmp	al,ah		;if two chars are not equal
-	jne	isFalse
+	jne	isFalse		;then not a palindrome
 	cmp	ecx, 1		;if copied over all chars it is a palindrome
-	jle	isTrue
-	jmp	reverse
+	jle	isTrue		;then is a palindomre
+	jmp	reverse		;keep comparing chars from each end
 isTrue:				;show result msg
 	PutStr	isPalindrome
-	nwln
-	jmp	END
+	nwln			;a new line
+	jmp	END		;end
 isFalse:
 	PutStr	notPalindrome	
-	nwln
-	jmp	END	
+	nwln			;a new line
+	jmp	END		;end
 END:	
-	.EXIT
+	.EXIT			;tell OS we are done
